6f516a913a1174178e4f4b5a643402754f116687
marvinwilliam <la805779602@hotmail.com>
2017-02-11 17:31:40 +0800

    使用fork的cdnizer.

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 var through = require("through2"),
-	gutil = require("gulp-util"),
+	gutil   = require("gulp-util"),
 	cdnizer = require('cdnizer');
 
 function pluginError(msg) {
 	return new gutil.PluginError("gulp-cdnizer", msg);
 }
 
-module.exports = function(opts) {
+module.exports = function (opts) {
 	"use strict";
-	
-	var cdnizerHandler = cdnizer(opts);
 
+	var cdnizerHandler = cdnizer(opts);
 
 	//noinspection JSUnusedLocalSymbols
 	function cdnizerStream(file, enc, callback) {
 
 		// Do nothing if no contents
-		if(file.isNull()) {
+		if (file.isNull()) {
 			this.push(file);
 			return callback();
 		}
 
-		if(file.isStream()) {
+		if (file.isStream()) {
 			this.emit("error", pluginError("Stream content is not supported"));
 			return callback();
 		}
 
 		// check if file.contents is a `Buffer`
-		if(file.isBuffer()) {
+		if (file.isBuffer()) {
 			try {
 				file.contents = new Buffer(cdnizerHandler(String(file.contents)));
 				this.push(file);
-			} catch(error) {
+			} catch (error) {
 				this.emit("error", pluginError(error.toString()))
 			}
 		}
